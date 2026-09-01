1b63f014af5bbbe30fed740bf50d59e66f0f46c6
lautiluna122-commits <lautiluna122@gmail.com>
2026-09-01 11:26:58 -0300

    feat(demo): expose commercial platform shell at /demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DemoPortal from './demo/DemoPortal';
 import './index.css';
+import './demo/demo.css';
+
+const isDemoPortal = window.location.pathname === '/demo' || window.location.pathname.startsWith('/demo/');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isDemoPortal ? <DemoPortal /> : <App />}
   </React.StrictMode>,
 );
